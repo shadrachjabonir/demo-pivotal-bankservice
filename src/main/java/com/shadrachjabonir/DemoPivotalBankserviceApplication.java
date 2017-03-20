5d753afa73f365b5be63149463ae43c1a2cbb7e4
shadrach <shadrachjabonir@gmail.com>
2017-03-20 21:29:23 +0700

    finisih

--- src/main/java/com/shadrachjabonir/DemoPivotalBankserviceApplication.java
+++ src/main/java/com/shadrachjabonir/DemoPivotalBankserviceApplication.java
@@ -1,13 +1,17 @@
 package com.shadrachjabonir;
 
 import com.netflix.discovery.converters.Auto;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
@@ -16,25 +20,35 @@ import java.io.Serializable;
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableAutoConfiguration
+@EnableCircuitBreaker
 @RestController
 public class DemoPivotalBankserviceApplication {
 
+//	@Value("${app.version}")
+//	String version;
+
 	@Autowired
-	BankDbClient bankDbClient;
+	BankService bankService;
+
+//	@CrossOrigin(origins = "*")
+//	@RequestMapping(value = "/getVersion",method = RequestMethod.GET)
+//	String getVersion(){
+//		return version;
+//	}
 
 	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "/getAccountDetail", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	Account getAccountDetail(@RequestBody Account account){
-		return bankDbClient.getAccount(account.getNumber());
+		return bankService.getAccount(account.getNumber());
 	}
 
 	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "/updateAccountBalance", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	Account updateAccountBalance(@RequestBody Account account){
-		Account willBeUpdated = bankDbClient.getAccount(account.getNumber());
+		Account willBeUpdated = bankService.getAccount(account.getNumber());
 		willBeUpdated.setAmount(account.getAmount());
 		System.out.println(willBeUpdated.getAmount());
-		willBeUpdated = bankDbClient.updateAccount(willBeUpdated.getId(),willBeUpdated);
+		willBeUpdated = bankService.updateAccount(willBeUpdated.getId(),willBeUpdated);
 		return willBeUpdated;
 	}
 
@@ -43,14 +57,49 @@ public class DemoPivotalBankserviceApplication {
 	}
 }
 
-@FeignClient(value = "bankDb", url = "https://demo-pivotal-bankdb.cfapps.io/")
+@FeignClient(value = "bankDb", url = "https://demo-pivotal-bankdb.cfapps.io")
 interface BankDbClient {
 
 	@RequestMapping(method = RequestMethod.GET, value = "/account/search/findByNumber?number={number}")
 	Account getAccount(@PathVariable("number") String number);
 
+	@HystrixCommand(fallbackMethod = "updateAccountFailed")
 	@RequestMapping(method = RequestMethod.PUT, value = "/account/{accountId}")
 	Account updateAccount(@PathVariable("accountId") long accountId, Account account);
+}
+
+@Service
+class BankService{
+
+	@Autowired
+	BankDbClient bankDbClient;
+
+	@HystrixCommand(fallbackMethod = "getAccountFailed")
+	public Account getAccount(String number) {
+		return bankDbClient.getAccount(number);
+	}
+
+	@HystrixCommand(fallbackMethod = "updateAccountFailed")
+	public Account updateAccount(long accountId, Account account) {
+		return bankDbClient.updateAccount(accountId,account);
+	}
+
+	Account failedAccount(){
+		Account account = new Account();
+		account.setId(0L);
+		account.setName("Failed");
+		account.setNumber("0000");
+		account.setAmount(0D);
+		return account;
+	}
+
+	Account getAccountFailed(String test) {
+		return failedAccount();
+	}
+
+	Account updateAccountFailed(long accountId, Account account) {
+		return failedAccount();
+	}
 }
 
 class Account implements Serializable {
